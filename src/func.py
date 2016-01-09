8559c93de6fdb3012000a3e976854ab3ed62818d
TakamichiToda56 <5toda6@gmail.com>
2016-01-09 22:29:16 +0900

    文字列のエンコード＆デコードの関数作成

--- src/func.py
+++ src/func.py
@@ -1,17 +1,29 @@
 import unittest
 
-def add2num(x,y):
-    return x + y
+def ascii2binary(word):
+    binary_list = list(map(hex, map(ord, word)))
+    return binary_list
+
+def binary2ascii(binary_list):
+    word = ""
+    for i in binary_list:
+        word += chr(int(i, 16))
+    return word
 
 class TestSequenceFunction(unittest.TestCase):
     '''
     unittestモジュールを使ったPythonのテストクラス
     '''
     
-    def test_equal(self):
-        x = 1
-        y = 2
-        self.assertEqual(add2num(x,y),3)
+    def test_encode_binary(self):
+        word = "hello"
+        binary_list = ['0x68', '0x65', '0x6c', '0x6c', '0x6f']
+        self.assertEqual(ascii2binary(word),binary_list)
+
+    def test_decode_word(self):
+        word = "hello"
+        binary_list = ['0x68', '0x65', '0x6c', '0x6c', '0x6f']
+        self.assertEqual(binary2ascii(binary_list),word)
 
 if __name__ == "__main__":
     unittest.main()
