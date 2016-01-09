16c57054afde216b4fe8df52c0b7966698567d34
TakamichiToda56 <5toda6@gmail.com>
2016-01-09 20:31:17 +0900

    URLのgetパラメータを取ってこれるようにした

--- src/first_app.py
+++ src/first_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,10 @@ def index():
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/login')
+def login():
+    return request.args.get('name','')
 
 if __name__ == '__main__':
     app.run()
