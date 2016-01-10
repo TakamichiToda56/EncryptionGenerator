b9a358f945fb54cd619d85f8307c06e8be05a780
TakamichiToda56 <5toda6@gmail.com>
2016-01-11 08:55:46 +0900

    暗号化部分完成

--- src/generator.py
+++ src/generator.py
@@ -3,12 +3,24 @@ from func import *
 
 app = Flask(__name__)
 
+key_num = 4
+
+@app.route('/')
+def index():
+    return 'hello'
+
 @app.route('/encryption/<word>')
 def encryption(word):
-    return word
+    binary_list = ascii2binary(word)
+    cipher_list = encryption_list(binary_list,key_num)
+    cipher = bind_cipher(cipher_list) 
+    return cipher
 
-@app.route('/decoding/<word>')
-def decoding(word):
+@app.route('/decoding/<cipher>')
+def decoding(cipher):
+    cipher_list = split_cipher(cipher)
+    binary_list = decode_list(cipher_list,key_num)
+    word = binary2ascii(binary_list)
     return word
 
 if __name__ == '__main__':
